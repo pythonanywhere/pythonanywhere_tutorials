02e5b596f69b4f9a76f6b996178207cb21c98866
Resolver Developers <developers@resolversystems.com>
2011-11-22 13:07:39 +0000

    Adds static directory and image files for django tutorial. By Giles & Hansel.

--- server/settings.py
+++ server/settings.py
@@ -58,6 +58,7 @@ MEDIA_URL = ''
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = ''
+STATIC_DOC_ROOT = '/home/tutorial/pythonanywhere_tutorials/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
--- server/urls.py
+++ server/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
 from server.renderer.views import render_view
-
+from server import settings
 
 urlpatterns = patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_DOC_ROOT, 'show_indexes': True}),
+        
     url(r'^(.*)$', render_view, name='render_view')
 )
+
+
